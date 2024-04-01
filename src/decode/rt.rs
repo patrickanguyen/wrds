7e22f2fa98bc729c1d21697ff58cdf5c01ede8d0
Patrick Nguyen <patrickanguyen1258@gmail.com>
2024-03-31 17:37:52 -0700

    Added RT decoding tests

--- src/decode/rt.rs
+++ src/decode/rt.rs
@@ -2,7 +2,7 @@
 
 use crate::types::{
     rt::{RadioTextSegment, TextAB, TextAddressCode},
-    Block2, Block3, Block4, ProgramIdentifier, TwoAPayload, TwoBPayload,
+    Block2, Block3, Block4, GroupType, ProgramIdentifier, TwoAPayload, TwoBPayload,
 };
 
 fn decode_block2(block: &Block2) -> (TextAB, TextAddressCode) {
@@ -24,8 +24,15 @@ fn decode_rt_segment(block: u16) -> RadioTextSegment {
     RadioTextSegment([letter1, letter2])
 }
 
-/// Decode RDS Message of Group Type 2A
+/// Decode RDS Message of Group Type 2A.
+/// Assumes that the Group Type has been already checked to be 2A.
 pub fn decode_2a(block2: &Block2, block3: &Block3, block4: &Block4) -> TwoAPayload {
+    debug_assert_eq!(
+        GroupType::n((block2.0 >> 11) as u8).unwrap(),
+        GroupType::TwoA,
+        "The Group Type must be 2A"
+    );
+
     let (text_ab, text_addr_code) = decode_block2(block2);
     let rt_segment = (decode_rt_segment(block3.0), decode_rt_segment(block4.0));
     TwoAPayload {
@@ -35,8 +42,15 @@ pub fn decode_2a(block2: &Block2, block3: &Block3, block4: &Block4) -> TwoAPaylo
     }
 }
 
-/// Decode RDS Message of Group Type 2B
+/// Decode RDS Message of Group Type 2B.
+/// Assumes that the Group Type has been already checked to be 2B.
 pub fn decode_2b(block2: &Block2, block3: &Block3, block4: &Block4) -> TwoBPayload {
+    debug_assert_eq!(
+        GroupType::n((block2.0 >> 11) as u8).unwrap(),
+        GroupType::TwoB,
+        "The Group Type must be 2B"
+    );
+
     let (text_ab, text_addr_code) = decode_block2(block2);
     let pi = ProgramIdentifier(block3.0);
     let rt_segment = decode_rt_segment(block4.0);
@@ -45,5 +59,46 @@ pub fn decode_2b(block2: &Block2, block3: &Block3, block4: &Block4) -> TwoBPaylo
         text_addr_code,
         pi,
         rt_segment,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        decode_2a, decode_2b, Block2, Block3, Block4, ProgramIdentifier, RadioTextSegment, TextAB,
+        TextAddressCode, TwoAPayload, TwoBPayload,
+    };
+
+    #[test]
+    fn two_a() {
+        let block2 = Block2(0x2000);
+        let block3 = Block3(0x3234);
+        let block4 = Block4(0x5678);
+
+        assert_eq!(
+            decode_2a(&block2, &block3, &block4),
+            TwoAPayload {
+                text_ab: TextAB(false),
+                text_addr_code: TextAddressCode(0),
+                rt_segment: (RadioTextSegment(['2', '4']), RadioTextSegment(['V', 'x']))
+            }
+        )
+    }
+
+    #[test]
+    fn two_b() {
+        let block2 = Block2(0x2A00);
+        let block3 = Block3(0x3234);
+        let block4 = Block4(0x5678);
+
+        assert_eq!(
+            decode_2b(&block2, &block3, &block4),
+            TwoBPayload {
+                text_ab: TextAB(false),
+                text_addr_code: TextAddressCode(0),
+                rt_segment: RadioTextSegment(['V', 'x']),
+                pi: ProgramIdentifier(0x3234)
+            }
+        )
     }
 }
