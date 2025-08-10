e82ceee5bc4372d2ca0ad7d8ebf2eaa6c633ec28
Patrick Nguyen <patrickanguyen1258@gmail.com>
2025-08-10 01:52:39 -0700

    Added thiserror

--- crates/wrds/src/error.rs
+++ crates/wrds/src/error.rs
@@ -1,9 +1,12 @@
 use crate::types::GroupType;
 
 /// Radio Data System Decoding Error
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, thiserror::Error)]
 pub enum Error {
+    #[error("Invalid input for field \"{field}\": `{value}`")]
     InvalidInput { field: &'static str, value: u16 },
+    #[error("Unimplemented RDS Group Type: {:?}", 0.0)]
     Unimplemented(GroupType),
+    #[error("Unknown error")]
     Unknown,
 }
